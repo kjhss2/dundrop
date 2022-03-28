e094ae983f1d5d817bc781c362dafbe15498ba31
musichh <kjhss2@nate.com>
2022-03-28 22:24:11 +0900

    * 누락된 비동기 관련 미들웨어 추가(redux-thunk)

--- src/redux/store/configureStore.dev.js
+++ src/redux/store/configureStore.dev.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
 
 /* Components */
 import rootReducer from '../reducers';
 import App from '../../App';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default function DevStore() {
   return (
--- src/redux/store/configureStore.prod.js
+++ src/redux/store/configureStore.prod.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 /* Components */
 import rootReducer from '../reducers';
 import App from '../../App';
 
-const store = createStore(rootReducer, applyMiddleware());
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function ProdStore() {
   return (
