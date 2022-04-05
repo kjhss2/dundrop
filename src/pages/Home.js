2dcc61037340de8162d72806ef765bc7aa62a206
jihun.kang <kjhss2@nate.com>
2022-04-05 15:05:07 +0900

    * Home 소개 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
     <>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <Box>
-          <Typography variant="h5">던드롭</Typography>
+          <Typography variant="h5">던드롭(DunDrop)</Typography>
         </Box>
         <Box sx={{ marginLeft: 2 }}>
           <Typography variant="h7">던전엔파이터 아이템 검색 사이트</Typography>
@@ -55,10 +55,13 @@ const Home = () => {
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
-          <Typography>안녕하세요. 소소하게 던전엔파이터를 즐기고 있는 유저입니다.</Typography>
-          <Typography>110레벨 확장으로 인한 105레벨 다양한 신규 아이템이 등장하였는데요.</Typography>
-          <Typography>좀 더 편하게 105레벨 아이템을 검색 할 수 있는 사이트를 만들어 보자 해서 만들게 되었습니다.</Typography>
-          <Typography>해당 사이트는 현재 베타로 운영중이며, 불편한 점 이 있으시면 문의주시면 감사하겠습니다.</Typography>
+          <Box>
+            <Typography>안녕하세요. 소소하게 던전엔파이터를 즐기고 있는 유저입니다.</Typography>
+            <Typography>110레벨 확장으로 인한 105레벨 다양한 신규 아이템이 등장하였는데요.</Typography>
+            <Typography>좀 더 편하게 105레벨 아이템을 검색 할 수 있는 사이트를 만들어 보자 해서 만들게 되었습니다.</Typography>
+            <br />
+            <Typography>해당 사이트는 현재 베타로 운영중이며, 불편한 점이 있으시면 문의주시면 감사하겠습니다.</Typography>
+          </Box>
         </TabPanel>
         <TabPanel value={value} index={1}>
           <List component="nav" aria-label="mailbox folders">
