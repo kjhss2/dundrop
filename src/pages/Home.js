06a4e5120737688025f68c43c56c8879c7de8485
musichh <kjhss2@nate.com>
2022-04-08 01:19:51 +0900

    * Home console warnnig fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -54,13 +54,11 @@ const Home = () => {
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
-          <Box>
-            <Typography>안녕하세요. 소소하게 던전엔파이터를 즐기고 있는 유저입니다.</Typography>
-            <Typography>110레벨 확장으로 인한 105레벨 다양한 신규 아이템이 등장하였는데요.</Typography>
-            <Typography>좀 더 편하게 105레벨 아이템을 검색 할 수 있는 사이트를 만들어 보자 해서 만들게 되었습니다.</Typography>
-            <br />
-            <Typography>해당 사이트는 현재 베타로 운영중이며, 불편한 점이 있으시면 문의주시면 감사하겠습니다.</Typography>
-          </Box>
+          안녕하세요. 소소하게 던전엔파이터를 즐기고 있는 유저입니다.<br />
+          110레벨 확장으로 인한 105레벨 다양한 신규 아이템이 등장하였는데요.<br />
+          좀 더 편하게 105레벨 아이템을 검색 할 수 있는 사이트를 만들어 보자 해서 만들게 되었습니다.<br />
+          <br />
+          해당 사이트는 현재 베타로 운영중이며, 불편한 점이 있으시면 문의주시면 감사하겠습니다.<br />
         </TabPanel>
         <TabPanel value={value} index={1}>
           <List component="nav" aria-label="mailbox folders">
