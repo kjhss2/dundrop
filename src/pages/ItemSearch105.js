8341a11a12203c6cb300a19bab58a1c5dd9ac53f
musichh <kjhss2@nate.com>
2022-04-08 00:49:11 +0900

    * 아이템 검색(105렙) 출처 링크 추가

--- src/pages/ItemSearch105.js
+++ src/pages/ItemSearch105.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
 
 // Components
@@ -13,6 +14,15 @@ const ItemSearch105 = () => {
     <>
       <SearchComponent />
       <SearchItems items={searchItems105} />
+      <Box>
+        던파공홈 언제나우린☆님 게시글 내용을 참고 하였습니다.&nbsp;
+        <a
+          href={'https://df.nexon.com/df/community/dnfboard?mode=view&no=2637550&job=99&grow_type=0#comment_050500000000000000000'}
+          target="_blank"
+          rel="noopener noreferrer">
+          출처링크
+        </a>
+      </Box>
     </>
   );
 };
