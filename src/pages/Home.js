3afd1db975bb36f4c3f263cb4eed192193e6ff0f
musichh <kjhss2@nate.com>
2022-04-10 00:00:15 +0900

    * 가이드 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { Box, Divider, List, ListItem, ListItemText, Tab, Tabs, Typography } from "@mui/material";
 import React from "react";
+import Guide from "../components/Guide";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -50,20 +51,28 @@ const Home = () => {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
             <Tab label="소개" {...a11yProps(0)} />
-            <Tab label="확인된 문제" {...a11yProps(1)} />
-            <Tab label="공지사항" {...a11yProps(2)} />
+            <Tab label="가이드" {...a11yProps(1)} />
+            <Tab label="확인된 문제" {...a11yProps(2)} />
+            <Tab label="공지사항" {...a11yProps(3)} />
           </Tabs>
         </Box>
+        <TabPanel value={value} index={1}>
+          <Guide />
+        </TabPanel>
         <TabPanel value={value} index={0}>
-          안녕하세요. 소소하게 던전엔파이터를 즐기고 있는 유저입니다.<br />
-          110레벨 확장으로 인한 105레벨 다양한 신규 아이템이 등장하였는데요.<br />
-          좀 더 편하게 105레벨 아이템을 검색 할 수 있는 사이트를 만들어 보자 해서 만들게 되었습니다.<br />
-          <br />
+          안녕하세요. 2006년부터 소소하게 던전엔파이터를 즐기고 있는 유저입니다.<br />
+          이번에 만렙 확장과 더불어 신규 105레벨 아이템을 좀 더 쉽게 검색하는 기능을 만들고자 사이트를 개발 하게 되었습니다.<br />
+          개발을 시작한지 얼마 안되서 부족한 점이 많지만 필요한 기능들을 추가할 예정입니다.<br />
           해당 사이트는 현재 베타로 운영중이며, 불편한 점이 있으시면 문의주시면 감사하겠습니다.<br />
+          <br />
+          ※ 주요 기능<br />
+          1. 105렙 아이템 Tag 검색 및 드랍 정보<br />
+          2. 보유 중인 아이템 표시 기능<br />
+          3. 캐릭터 아이템 획득 이력<br />
           <br />
           현재 사이트는 PC화면(넓이 1000px 이상)에서 최적화된 화면을 보실 수 있으며 모바일에서도 보시기 불편함이 없게 수정 예정입니다.
         </TabPanel>
-        <TabPanel value={value} index={1}>
+        <TabPanel value={value} index={2}>
           <List component="nav" aria-label="mailbox folders">
             <ListItem>
               <ListItemText primary="상자 정가로 회득한 템이 보유 아이템에 표시 되지 않는 현상(수정완료)" secondary="2020.04.09 | 아이템 타임라인 항아리정가,초월 추가" />
@@ -71,7 +80,7 @@ const Home = () => {
             <Divider />
           </List>
         </TabPanel>
-        <TabPanel value={value} index={2}>
+        <TabPanel value={value} index={3}>
           <List component="nav" aria-label="mailbox folders">
             <ListItem>
               <ListItemText primary="노블레스 코드 캐릭터별 보유 아이템 표시 기능" secondary="업데이트 예정" />
